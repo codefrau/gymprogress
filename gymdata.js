7ea07e586c69da24765c122e0d5335c032f7bb15
Cal1baN666 <93940687+Cal1baN666@users.noreply.github.com>
2021-12-07 01:20:27 +0100

    Update gymdata.js
    1 neue Arena

--- gymdata.js
+++ gymdata.js
@@ -474,6 +474,7 @@ function gymData() {
     {name: "Partnerstädte von Magdeburg", district: "Berliner Chaussee", address: "Berliner Chaussee", location: [52.135017,11.713523]},
     {name: "Sportplatz SSV Germania 1990 e. V Magdeburg", district: "Berliner Chaussee", address: "Zum Waldsee 11", location: [52.136286,11.712626]},
     {name: "Pumpe Harzburger Straße", district: "Lemsdorf", address: "BHarzburger Str. 1", location: [52.098407,11.597171]},
+    {name: "Roter Hirsch", district: "Berliner Chaussee", address: "Zum Friedensweiler 12", location: [52.136900, 11.710400]},
     //{name: "", district: "", address: "", location: []},
   ],
     // local names for Level 12 cells
