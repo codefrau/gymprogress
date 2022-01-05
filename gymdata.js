960f99e4ed9a750250f952683bea9038651b4272
Cal1baN666 <93940687+Cal1baN666@users.noreply.github.com>
2022-01-05 19:10:44 +0100

    Update gymdata.js
    2 neue Arenen

--- gymdata.js
+++ gymdata.js
@@ -473,8 +473,10 @@ function gymData() {
     {name: "Mein Baum für Magdeburg - Erinnerungen", district: "Berliner Chaussee", address: "Berliner Chaussee", location: [52.134333,11.711378]},
     {name: "Partnerstädte von Magdeburg", district: "Berliner Chaussee", address: "Berliner Chaussee", location: [52.135017,11.713523]},
     {name: "Sportplatz SSV Germania 1990 e. V Magdeburg", district: "Berliner Chaussee", address: "Zum Waldsee 11", location: [52.136286,11.712626]},
-    {name: "Pumpe Harzburger Straße", district: "Lemsdorf", address: "BHarzburger Str. 1", location: [52.098407,11.597171]},
+    {name: "Pumpe Harzburger Straße", district: "Lemsdorf", address: "Harzburger Str. 1", location: [52.098407,11.597171]},
     {deleted: true, name: "Roter Hirsch", district: "Berliner Chaussee", address: "Zum Friedensweiler 12", location: [52.136900, 11.710400]},
+    {name: "Kletterbirne", district: "Ottersleben", address: "Pfirsichweg", location: [52.092368,11.557586]},
+    {name: "Spielplatz Neustädter Feld", district: "Neustädter Feld", address: "Mechthildstraße 48", location: [52.157241,11.622686]},
     //{name: "", district: "", address: "", location: []},
   ],
     // local names for Level 12 cells
