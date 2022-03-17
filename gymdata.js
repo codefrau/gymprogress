28fe6c5a6f2bb7a980ecaacda3a3609a533745fc
Cal1baN666 <93940687+Cal1baN666@users.noreply.github.com>
2022-03-17 01:51:06 +0100

    Update gymdata.js
    1 neue Arena

--- gymdata.js
+++ gymdata.js
@@ -484,6 +484,7 @@ function gymData() {
     {name: "Gedenkstein Werner von Siemens Ring", district: "Ottersleben", address: "Werner-von-Siemens-Ring 1", location: [52.083341,11.582386]},
     {name: "Stolperstein Anna Bertha Jordan", district: "Ottersleben", address: "Alt Benneckenbeck 23", location: [52.085365,11.58435]},
     {name: "Gartenverein „Naturfreunde“ e.V.", district: "Sudenburg", address: "Thomas-Müntzer-Straße 26", location: [52.106253,11.579467]},
+    {name: "Spielplatz Hugo Junkers Straße", district: "Nordwest", address: "Hugo-Junkers-Allee 94A", location: [52.150006,11.598882]},
       
     //{name: "", district: "", address: "", location: []},
   ],
